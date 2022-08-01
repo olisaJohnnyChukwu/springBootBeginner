436e9dc3381e250a2d933ad57a04c4b678b91a45
olisaJohnnyChukwu <ojohnnyc@depaul.edu>
2022-07-31 23:40:05 -0400

    post gre set up

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,14 +1,23 @@
 package com.example.demo.student;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.*;
 
 @Data
 @NoArgsConstructor
 @Getter
 @Setter
-
+@Entity
+@Table
 public class Student {
+    @Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
